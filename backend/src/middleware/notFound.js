b94f4d97a68db1000ec1e562b8c2ad14414605d9
RISHABH KUSHWAHA <rbkush101@gmail.com>
2026-05-28 01:49:45 +0530

    Delete backend directory

--- backend/src/middleware/notFound.js
+++
@@ -1,6 +0,0 @@
-function notFound(req, res) {
-    res.status(404).json({ success: false, message: `Route not found: ${req.method} ${req.originalUrl}` });
-}
-
-module.exports = { notFound };
-
--- backend/src/models/index.js
+++
@@ -1,9 +0,0 @@
-const Student = require('./Student');
-const Teacher = require('./Teacher');
-const Fee = require('./Fee');
-const Payment = require('./Payment');
-const Salary = require('./Salary');
-const Attendance = require('./Attendance');
-
-module.exports = { Student, Teacher, Fee, Payment, Salary, Attendance };
-
